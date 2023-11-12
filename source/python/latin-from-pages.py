4f7d5cbe9d427d6bd5b11416117c7d9e8877d25c
Marc Moskowitz <marc@suberic.net>
2023-11-12 14:04:24 -0500

    Initial data prep work.

---
+++ source/python/latin-from-pages.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys,re
+
+filename = sys.argv[1]
+
+title = ''
+
+output_lines = []
+is_text = False
+is_latin = False
+
+#read content
+with open(filename) as file:
+    for line in file:
+        if (is_text):
+            if (is_latin):
+                if (re.match('^==[^=]+==.*', line)):
+                    is_latin = False
+                    break
+                elif (line.find("</text") >= 0):
+                    output_lines.append(line[0:line.find("</text")])
+                    break
+                      
+                else:
+                    output_lines.append(line)
+            else:
+                if (line.startswith('==Latin==')):
+                    is_latin=True
+                    output_lines.append(line)
+                    
+        else: # is not text
+            match = re.search("<title>(.*)</title>", line)
+            if (match is not None):
+                title = match.group(1)
+            elif (line.find("<text ") >= 0): #start text
+                is_text = True
+                if (line.find("==Latin==") >= 0):
+                    is_latin=True
+                    output_lines.append("==Latin==")
+                    
+with open(title.replace(' ', '_') + ".txt", "w") as output:
+    output.write("=Lemma:=" + title + "\n")
+    for line in output_lines:
+        output.write(line)
+                    
+                    
+    
+
+            
